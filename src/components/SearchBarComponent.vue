86cf36891d2f2fd89950672807b20da3ef5f0e22
Moises <ve11621@innovaccion.mx>
2022-08-19 20:42:57 -0500

    Search suggestions for search bar finished, I need to improve my css skills later, for now It's okay, so with the suggestions I must apply debounce for fetching data optimization, that'll be later. Now I'll focus on backend

--- src/components/SearchBarComponent.vue
+++ src/components/SearchBarComponent.vue
@@ -1,19 +1,56 @@
 <style scoped>
-@media (max-width:800px){
-    .navbar{
-        display: flex !important;
-        flex-direction: column !important;
-    }
 
-    .search-main-container{
+.suggestions-block-helper {
+    position: absolute;
+    z-index: 90000;
+    top: 2.85rem;
+    right: 0;
+    width: 50%;
+    overflow-y: auto;
+    height: 15rem;
+}
+
+.suggestion-name {
+    border: 1px solid black;
+    padding: 2px;
+    width: 100%;
+    height: 15%;
+}
+
+@media (max-width:800px) {
+    
+    .search-main-container {
         width: 100% !important;
     }
+
+    .suggestions-block-helper {
+        position: absolute;
+        z-index: 90000;
+        top: 5.45rem !important;
+        right: 0;
+        left: 0;
+        width: 100% !important;
+        overflow-y: auto;
+        height: 15rem;
+    }
+
+    .suggestion-name {
+        padding: 2px;
+        width: 100%;
+        height: 15%;
+    }
 }
+
 </style>
 <template>
-    <div class="search-main-container w-50">
+    <div class="search-main-container d-flex flex-column w-50">
         <div class="search-users-container d-flex flex-row w-100">
-            <input type="text" class="form-control w-100" placeholder="Introduce el nombre de usuario...">
+            <div class="suggestions-container w-100 d-flex flex-column">
+                <input type="text" class="form-control w-100" placeholder="Introduce el nombre de usuario...">
+                <div class="suggestions-block-helper d-flex flex-column">
+                    <div class="suggestion-name">asdasda</div>
+                </div>
+            </div>
             <input type="button" class="btn btn-primary" value="Buscar usuario">
         </div>
     </div>
